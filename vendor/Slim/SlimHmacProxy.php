26a535cd33a24135a3d7eaffd1561fd2d538c754
kuisatz <kuisatzhaderah@gmail.com>
2015-12-15 15:55:25 +0200

    leftnavigation yaratıldı. cağırılacak servisler tanımlandı

--- vendor/Slim/SlimHmacProxy.php
+++ vendor/Slim/SlimHmacProxy.php
@@ -3,45 +3,54 @@
 namespace vendor\Slim;
 
 class SlimHmacProxy extends \vendor\Proxy\Proxy {
-    
-     /**
+
+    /**
      * a mapping array for related functions and proxy calls
      * to be executed
      * @var array()
      */
     protected $redirectMap = array('getReports_test' => 'restApiDefaultCall',
-                                   'getDynamicForm_test' => 'restApiDefaultCall',);
-    
+                                    'getDynamicForm_test' => 'restApiDefaultCall',
+                                    'delete_leftnavigation' => 'restApiDefaultCall',
+                                    'getAll_leftnavigation' => 'restApiDefaultCall',
+                                    'insert_leftnavigation' => 'restApiDefaultCall',
+                                    'update_leftnavigation' => 'restApiDefaultCall',
+                                    'fillGrid_leftnavigation' => 'restApiDefaultCall',
+                                    'fillGridRowTotalCount_leftnavigation' => 'restApiDefaultCall',
+                                    'getLeftMenu_leftnavigation' => 'restApiDefaultCall',
+        
+    );
+
     /**
      * hmac object
      * @var \vendor\hmac\Hmac
      */
     protected $hmacObj;
-    
-    
+
     public function __construct() {
         parent::__construct();
         $this->hmacObj = new \vendor\hmac\Hmac();
     }
-    
+
     public function redirect() {
         $execFunction = $this->resolveRedirectMap();
-        $this->setEndPointByClosure();  
+        $this->setEndPointByClosure();
         echo $this->$execFunction();
     }
-    
-     /**
+
+    /**
      * Rest api 'GET' call (Curl lib)
      * function overriden
      * @author Mustafa Zeynel Dağlı
      * @version 0.2
      */
     public function restApiDefaultCall() {
-        /*$encrypt = new \vendor\Encrypt\EncryptManual('test');
-        $encryptValue = $encrypt->encrypt_times(4, 'kullanici:sifre');
-        //print_r('--'.$encryptValue.'--');
-        $decryptValue = $encrypt->decrypt_times(4, $encryptValue);
-        //print_r('??'.$decryptValue.'??');*/
+                
+        /* $encrypt = new \vendor\Encrypt\EncryptManual('test');
+          $encryptValue = $encrypt->encrypt_times(4, 'kullanici:sifre');
+          //print_r('--'.$encryptValue.'--');
+          $decryptValue = $encrypt->decrypt_times(4, $encryptValue);
+          //print_r('??'.$decryptValue.'??'); */
         $this->setEncryptClass();
         $params = null;
         $params = $this->getRequestParams();
@@ -50,7 +59,7 @@ class SlimHmacProxy extends \vendor\Proxy\Proxy {
         $this->hmacObj->setRequestParams($this->getRequestParamsWithoutPublicKey());
         $this->hmacObj->makeHmac();
         //print_r($this->hmacObj);
-        
+
         $preparedParams = $this->prepareGetParams();
         //$preparedParams = $this->prepareGetParams('', array('pk'));
         if (($ch = @curl_init()) == false) {
@@ -59,41 +68,41 @@ class SlimHmacProxy extends \vendor\Proxy\Proxy {
         }
         //print_r($this->restApiFullPathUrl.'?'.$preparedParams);
         //print_r($this->endPointUrl.$this->getEndPointFunction().'?'.$preparedParams);
-        curl_setopt($ch, CURLOPT_URL, $this->restApiFullPathUrl.'?'.$preparedParams ); //Url together with parameters
+        curl_setopt($ch, CURLOPT_URL, $this->restApiFullPathUrl . '?' . $preparedParams); //Url together with parameters
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Return data instead printing directly in Browser
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , $this->getCallTimeOut()); //Timeout (Default 7 seconds)
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->getCallTimeOut()); //Timeout (Default 7 seconds)
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'X-Public: '.$this->hmacObj->getPublicKey().'',
-            'X-Hash: '.$this->hmacObj->getHash().'',
-            'X-Nonce:'.$this->hmacObj->getNonce(),
+            'X-Public: ' . $this->hmacObj->getPublicKey() . '',
+            'X-Hash: ' . $this->hmacObj->getHash() . '',
+            'X-Nonce:' . $this->hmacObj->getNonce(),
             //'X-IP:'.serialize($_SERVER),
-            'X-TimeStamp:'.$this->hmacObj->setTimeStamp($this->encryptClass
-                                                             ->encrypt(''.time().''))  /// replay attack lar için oki
+            'X-TimeStamp:' . $this->hmacObj->setTimeStamp($this->encryptClass
+                            ->encrypt('' . time() . ''))  /// replay attack lar için oki
         ));
         curl_setopt($ch, CURLOPT_HEADER, 0); // we don’t want also to get the header information that we receive.
         //sleep(10);
         $response = curl_exec($ch);
-        $http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
-    
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
         if ($response == false) {
             die("curl_exec() failed. Error: " . curl_error($ch));
         }
- 
+
         return $response;
     }
-    
-    public function setEndPointByClosure(Array $EndPointClosure=null) {
+
+    public function setEndPointByClosure(Array $EndPointClosure = null) {
         $endPointFunction = $this->getRestApiEndPointFunction();
         if (substr($endPointFunction, -5) == '_test') {
             //$this->setEndPointUrl("http://localhost/slim2_test/index.php/");
-            $this->setRestApiFullPathUrl($this->restApiBaseUrl.$this->restApiEndPoint.$this->restApiEndPointFunction);
-        }else {
-            //$this->setEndPointUrl("http://localhost/slim2_test/".$this->getInvalidCallUrl()."/");
-            //$this->setEndPointUrl("".$this->getRestApiBaseUrl()."/".$this->getInvalidCallUrl()."/");
-            //$this->setEndPointFunction($this->getInvalidCallFunc());
+            $this->setRestApiFullPathUrl($this->restApiBaseUrl . $this->restApiEndPoint . $this->restApiEndPointFunction);
+        } else if (substr($endPointFunction, -15) == '_leftnavigation') {
+            $this->setRestApiEndPoint('leftnavigation.php/');
+            print_r($this->restApiBaseUrl.$this->restApiEndPoint.$this->restApiEndPointFunction);
+            $this->setRestApiFullPathUrl($this->restApiBaseUrl . $this->restApiEndPoint . $this->restApiEndPointFunction);
         }
     }
-    
+
     /**
      * set Hmac object for HMAC security
      * @param \vendor\hmac\Hmac $hmacObj
@@ -103,7 +112,7 @@ class SlimHmacProxy extends \vendor\Proxy\Proxy {
     public function setHmacObj(\vendor\hmac\Hmac $hmacObj) {
         $this->hmacObj = $hmacObj;
     }
-    
+
     /**
      * get Hmac object for HMAC security
      * @return \vendor\hmac\Hmac $hmacObj
@@ -113,6 +122,5 @@ class SlimHmacProxy extends \vendor\Proxy\Proxy {
     public function getHmacObj() {
         return $this->hmacObj;
     }
-    
-}
 
+}
